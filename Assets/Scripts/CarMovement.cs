b2c050030971ee3f4ed691c098075265e0214d83
hosoji <ha1249@nyu.edu>
2022-03-17 20:42:31 -0400

    Frogger project added; assets, scripts

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+
+
+
+    void Update()
+    {
+
+        //Hint: In Unity You can get Vector (direction) shorthands like this:
+        //Vector3.up (etc)
+        
+    }
+}
